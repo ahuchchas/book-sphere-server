9d4d64f5d608f6b9e9ac4b1466cc130cb182a2e4
Abu Hurayra Uchchas <ahuchchas@outlook.com>
2023-11-29 11:04:02 +0600

    rolled back

--- index.js
+++ index.js
@@ -92,6 +92,8 @@ async function run() {
       }
     });
   } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 run().catch(console.dir);
